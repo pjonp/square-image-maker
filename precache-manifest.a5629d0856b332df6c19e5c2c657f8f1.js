c9d1d0929e45f046dcd1aaabbee8e7b4646e1fc3
pjonp <pjonp01@gmail.com>
2020-06-08 19:11:39 -0400

    Updates

---
+++ precache-manifest.a5629d0856b332df6c19e5c2c657f8f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a1d480eede9af5fe45966a807a6b18",
+    "url": "/square-image-maker/index.html"
+  },
+  {
+    "revision": "b1635d0c8971860e444d",
+    "url": "/square-image-maker/static/css/2.34dcc6c8.chunk.css"
+  },
+  {
+    "revision": "43c77c1661aaa1497205",
+    "url": "/square-image-maker/static/css/main.9067bf5c.chunk.css"
+  },
+  {
+    "revision": "b1635d0c8971860e444d",
+    "url": "/square-image-maker/static/js/2.4f0d200e.chunk.js"
+  },
+  {
+    "revision": "2901baf57747062bcf45ab726832dae1",
+    "url": "/square-image-maker/static/js/2.4f0d200e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c77c1661aaa1497205",
+    "url": "/square-image-maker/static/js/main.03a970a8.chunk.js"
+  },
+  {
+    "revision": "0b238d1dcab3e27de242",
+    "url": "/square-image-maker/static/js/runtime-main.6a0d3184.js"
+  },
+  {
+    "revision": "a82040ecf06139df2f3e653326b3420c",
+    "url": "/square-image-maker/static/media/Bangers-Regular.a82040ec.ttf"
+  }
+]);
